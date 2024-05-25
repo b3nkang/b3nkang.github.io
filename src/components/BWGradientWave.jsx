e11b9fa80af2e15edfab224adc483d05a4d7cdd5
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-05-25 03:30:12 -0700

    add spacer removal on final wave

--- src/components/BWGradientWave.jsx
+++ src/components/BWGradientWave.jsx
@@ -9,11 +9,11 @@ export default function BWGradientWave({indexMod =1, spacerHeight=5, colorArray=
         <>     
             { 
                 colorArray.map((color, index) => {
-                    const waveClassName = index === 0 ? "fix" : null;
+                    const isLast = index === colorArray.length -1 ? true : false
                     return (
-                        <div key={index} className={waveClassName} style={{ marginBottom: (index === colorArray.length - 1) ? "0px" : "-750px" }}>
+                        <div key={index} style={{ marginBottom: (index === colorArray.length - 1) ? "0px" : "-750px" }}>
                             <Wave color={color} />
-                            <SpacerWave color={color} height={spacerHeight} />
+                            {!isLast && <SpacerWave color={color} height={spacerHeight} />}
                         </div>
                     );
                 })
@@ -21,23 +21,3 @@ export default function BWGradientWave({indexMod =1, spacerHeight=5, colorArray=
         </>
     )
 }
-
-// const svgPath = "M0,50 Q25,0 50,50 T100,50";
-// const svgPath = "M20,50 S30,40 40,50 S70,70 80,40"
-// const svgPath = "M20,100 S30,-10 40,100 S70,120 80,-10"
-// const svgPath = "M0,100 S20,-200 30,100 S90,120 100,-10"
-
-
-// export default function BWGradient({ indexMod = 1, spacerHeight = 10, colorArray = [bwgrad2, bwgrad3, bwgrad4, bwgrad5, bwgrad6, bwgrad7, bwgrad8, bwgrad9, bwgrad10, bwgrad11, bwgrad12, bwgrad13, bwgrad14, bwgrad15, bwgrad16, bwgrad17, bwgrad18, bwgrad19, bwgrad20] }) {
-//   return (
-//     <>     
-//       { 
-//         colorArray.map((color, index) => (
-//           index % indexMod === 0 ? (
-//             <WavySpacer key={index} color={`rgba(${color.r},${color.g},${color.b},${color.a})`} height={spacerHeight} svgPath={svgPath} />
-//           ) : null
-//         ))
-//       }
-//     </>
-//   );
-// }
