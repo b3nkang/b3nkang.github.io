1f96f1dbca28a1271d2ec1f61f04d3bc910b9123
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-05-29 23:54:05 -0700

    begin square layering

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -14,7 +14,7 @@ function Experience({nextSectionRef, color = {r:0,g:0,b:0,a:1.0,}}){
                 if (nextSectionRef.current.getBoundingClientRect().top < 0) {
                     setRefStyling({ 
                         backgroundColor: 'rgba(255, 255, 255, 1.0)', 
-                        boxShadow: `0px 0px 0px 30px rgba(255, 255, 255, 1.0)`
+                        // boxShadow: `0px 0px 0px 30px rgba(255, 255, 255, 1.0)`
                     });
                     console.log("Set ref styling to grey");
                 } else {
@@ -36,7 +36,9 @@ function Experience({nextSectionRef, color = {r:0,g:0,b:0,a:1.0,}}){
     return(
         <div className="panelEd" style={divStyling}>
             <div className={`expPanel`} style={refStyling}>
-                <h1>past experience</h1>
+                <div className="wave1">
+                    <h1>past experience</h1>
+                </div>
             </div>
         </div>
     )
