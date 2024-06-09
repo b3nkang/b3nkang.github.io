760f3127d84553567cd8c931079160f4513fd356
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-06-09 01:27:47 -0700

    refactor Exp away from MC

--- src/MainCard.jsx
+++ src/MainCard.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from "react"
 import './App.css'
-import ExperienceItem from "./components/ExperienceItem"
-import EducationItem from "./components/EducationItem"
-import LanguageButton from "./components/LanguageButton"
-import Skills from "./Skills"
 import Education from "./Education"
+import Experience from "./Experience"
+import Skills from "./Skills"
 import WaveSquare from "./components/WaveSquare"
 
 export default function MainCard({nextSectionRef, color = {r:0,g:0,b:0,a:1.0,}}){
@@ -110,51 +108,7 @@ export default function MainCard({nextSectionRef, color = {r:0,g:0,b:0,a:1.0,}})
                                     </div>
                                 </div>
                                 {/* <div className="expCont"><h2>past experience</h2></div> */}
-                                <div className="wave4" style={{ marginTop: "20px", ...wave4Styling}}>
-                                    <ExperienceItem
-                                        imgSrc={"/src/assets/images/intus_care_logo.jpeg"} 
-                                        alt={"Intus Care logo"}
-                                        name="Intus Care"
-                                        title="Software Engineering Intern"
-                                        period="Jun. 2024 – Aug. 2024"
-                                        location="Cambridge, MA"
-                                        desc="Incoming for summer 2024, working on applications team with MongoDB, React, Zod (data validation), Zustand (state manager), and TypeScript. Developing tRPC protocol routes and data modelling for seeding MongoDB instances."
-                                        iconStyling={iconStyling}
-                                    />
-                                    <ExperienceItem 
-                                        imgSrc={"/src/assets/images/usereferme_logo.jpeg"} 
-                                        alt={"Refer Me logo"}
-                                        name="Refer Me"
-                                        title="Software Engineering Intern"
-                                        period="Feb. 2024 – Apr. 2024"
-                                        location={`(Remote) Seattle, WA`}
-                                        desc="Engineered job scraper with bs4/Selenium, utilizing instructor & Pydantic to extract JSON via OpenAI's API. Reduced scraper API costs by 10^3% ($0.01 to $0.0005/call) via chunking inputs and model downgrades while holding 98% accuracy."
-                                        iconStyling={iconStyling}
-                                    />
-                                    <ExperienceItem 
-                                        imgSrc={"/src/assets/images/letsift_logo.jpeg"} 
-                                        alt={"Sift logo"}
-                                        name="Sift"
-                                        title="Software Engineering Intern"
-                                        period="Nov. 2023 – Jan. 2024"
-                                        location="Providence, RI"
-                                        desc="Led homepage migration to React/TypeScript/Next.js stack, deprecating thousands of lines of code. Optimized site performance with React Lazy Load, Next.js Image and server-side rendering to achieve Lighthouse SEO score of 100."
-                                        iconStyling={iconStyling}
-                                    />
-                                    <ExperienceItem
-                                        imgSrc={"/src/assets/images/cims_logo.jpeg"} 
-                                        alt={"NYU Courant logo"}
-                                        name="AI & Predictive Analytics Lab"
-                                        title="Visiting Researcher, NYU Courant"
-                                        period="June 2023 – Oct. 2023"
-                                        location="New York, NY"
-                                        desc="Built world’s largest corpus of queryable LENR research on AI & Low-Energy Nuclear Reactions (AI-LENR) team. Co-authored paper in IEEE ICBDA 2024 and was main contributor to LENRdashboard.com, built with D3.js, JS, HTML/CSS."
-                                        bottomPadding={0}
-                                        mt={4.5}
-                                        mb={0}
-                                        iconStyling={iconStyling}
-                                    />  
-                                </div>
+                                <Experience iconStyling={iconStyling} waveStyling={wave4Styling} />
                             </WaveSquare>
                         </WaveSquare>
                     </WaveSquare>
