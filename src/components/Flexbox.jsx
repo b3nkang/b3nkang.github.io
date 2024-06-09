433ed3dbf64f9cbaa10eec3b03f330f9fd171534
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-06-09 11:40:24 -0700

    refactor flex into component

---
+++ src/components/Flexbox.jsx
@@ -0,0 +1,28 @@
+export default function Flexbox({
+    fG,
+    fB,
+    fD,
+    justifyContent,
+    alignItems,
+    gap=0,
+    pl=0,
+    pr=0,
+    pt=0,
+    pb=0,
+    children
+}) {
+    return (
+        <div style={{
+            display: 'flex',
+            flexGrow: fG,
+            flexBasis: `${fB}%`,
+            flexDirection: fD,
+            justifyContent: justifyContent,
+            alignItems: alignItems,
+            gap: gap,
+            padding: `${pt}px ${pr}px ${pb}px ${pl}px`,
+        }}>
+            {children}
+        </div>
+    )
+}
