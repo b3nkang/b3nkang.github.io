78bac221dbc2fe63dcdb32c7b73d24bb1f83fd44
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-06-09 22:08:46 -0700

    update HeaderTypography with extra attr

--- src/components/HeaderTypography.jsx
+++ src/components/HeaderTypography.jsx
@@ -1,8 +1,8 @@
 import '../App.css'
 
-function HeaderTypography({ children, headerClass, pl=0, mb=0}) {
+function HeaderTypography({ children, headerClass, pl=0, mb=0, mt=0}) {
     return (
-        <div className={headerClass}  style={{ display: "flex", justifyContent: "start", paddingLeft: `${pl}px`, marginBottom: `${mb}px` }}><h2>{children}</h2></div>
+        <div className={headerClass}  style={{ display: "flex", justifyContent: "start", paddingLeft: `${pl}px`, marginBottom: `${mb}px`, marginTop: `${mt}px`}}><h2>{children}</h2></div>
     )
 }
 
