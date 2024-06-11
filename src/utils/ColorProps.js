17d4dc44c8d9c6b3034bcc1a011c404d625c668e
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-06-10 17:17:59 -0700

    set main ColorProps to new aliceblue palette

--- src/utils/ColorProps.js
+++ src/utils/ColorProps.js
@@ -1,4 +1,4 @@
-// bwgrad1 = white, until bwgrad20 = black
+// src/utils/ColorProps.js
 
 export const transpWhite = {
   r: 255,
@@ -8,134 +8,134 @@ export const transpWhite = {
 };
 
 export const bwgrad1 = {
-  r: 255,
-  g: 255,
+  r: 240,
+  g: 248,
   b: 255,
   a: 1.0,
 };
 
 export const bwgrad2 = {
-  r: 242,
-  g: 242,
+  r: 227,
+  g: 235,
   b: 242,
   a: 1.0,
 };
 
 export const bwgrad3 = {
-  r: 229,
-  g: 229,
+  r: 214,
+  g: 222,
   b: 229,
   a: 1.0,
 };
 
 export const bwgrad4 = {
-  r: 216,
-  g: 216,
+  r: 201,
+  g: 209,
   b: 216,
   a: 1.0,
 };
 
 export const bwgrad5 = {
-  r: 203,
-  g: 203,
+  r: 188,
+  g: 196,
   b: 203,
   a: 1.0,
 };
 
 export const bwgrad6 = {
-  r: 190,
-  g: 190,
+  r: 175,
+  g: 183,
   b: 190,
   a: 1.0,
 };
 
 export const bwgrad7 = {
-  r: 177,
-  g: 177,
+  r: 162,
+  g: 170,
   b: 177,
   a: 1.0,
 };
 
 export const bwgrad8 = {
-  r: 164,
-  g: 164,
+  r: 149,
+  g: 157,
   b: 164,
   a: 1.0,
 };
 
 export const bwgrad9 = {
-  r: 151,
-  g: 151,
+  r: 136,
+  g: 144,
   b: 151,
   a: 1.0,
 };
 
 export const bwgrad10 = {
-  r: 138,
-  g: 138,
+  r: 123,
+  g: 131,
   b: 138,
   a: 1.0,
 };
 
 export const bwgrad11 = {
-  r: 125,
-  g: 125,
+  r: 110,
+  g: 118,
   b: 125,
   a: 1.0,
 };
 
 export const bwgrad12 = {
-  r: 112,
-  g: 112,
+  r: 97,
+  g: 105,
   b: 112,
   a: 1.0,
 };
 
 export const bwgrad13 = {
-  r: 99,
-  g: 99,
+  r: 84,
+  g: 92,
   b: 99,
   a: 1.0,
 };
 
 export const bwgrad14 = {
-  r: 86,
-  g: 86,
+  r: 71,
+  g: 79,
   b: 86,
   a: 1.0,
 };
 
 export const bwgrad15 = {
-  r: 73,
-  g: 73,
+  r: 58,
+  g: 66,
   b: 73,
   a: 1.0,
 };
 
 export const bwgrad16 = {
-  r: 60,
-  g: 60,
+  r: 45,
+  g: 53,
   b: 60,
   a: 1.0,
 };
 
 export const bwgrad17 = {
-  r: 47,
-  g: 47,
+  r: 32,
+  g: 40,
   b: 47,
   a: 1.0,
 };
 
 export const bwgrad18 = {
-  r: 34,
-  g: 34,
+  r: 19,
+  g: 27,
   b: 34,
   a: 1.0,
 };
 
 export const bwgrad19 = {
-  r: 21,
-  g: 21,
+  r: 6,
+  g: 14,
   b: 21,
   a: 1.0,
 };
@@ -161,10 +161,11 @@ export const green = {
   a: 1.0,
 };
 
+// // bwgrad1 = white, until bwgrad20 = black
 
 // export const transpWhite = {
-//   r: 240,
-//   g: 248,
+//   r: 255,
+//   g: 255,
 //   b: 255,
 //   a: 0,
 // };
@@ -177,129 +178,128 @@ export const green = {
 // };
 
 // export const bwgrad2 = {
-//   r: 225,
-//   g: 234,
+//   r: 242,
+//   g: 242,
 //   b: 242,
 //   a: 1.0,
 // };
 
 // export const bwgrad3 = {
-//   r: 204,
-//   g: 215,
-//   b: 227,
+//   r: 229,
+//   g: 229,
+//   b: 229,
 //   a: 1.0,
 // };
 
 // export const bwgrad4 = {
-//   r: 197,
-//   g: 209,
-//   b: 222,
+//   r: 216,
+//   g: 216,
+//   b: 216,
 //   a: 1.0,
 // };
 
 // export const bwgrad5 = {
-//   r: 184,
-//   g: 197,
-//   b: 212,
+//   r: 203,
+//   g: 203,
+//   b: 203,
 //   a: 1.0,
 // };
 
 // export const bwgrad6 = {
-//   r: 156,
-//   g: 170,
-//   b: 186,
+//   r: 190,
+//   g: 190,
+//   b: 190,
 //   a: 1.0,
 // };
 
 // export const bwgrad7 = {
-//   r: 129,
-//   g: 144,
-//   b: 161,
+//   r: 177,
+//   g: 177,
+//   b: 177,
 //   a: 1.0,
 // };
 
 // export const bwgrad8 = {
-//   r: 103,
-//   g: 118,
-//   b: 135,
+//   r: 164,
+//   g: 164,
+//   b: 164,
 //   a: 1.0,
 // };
 
 // export const bwgrad9 = {
-//   r: 85,
-//   g: 99,
-//   b: 115,
+//   r: 151,
+//   g: 151,
+//   b: 151,
 //   a: 1.0,
 // };
 
 // export const bwgrad10 = {
-//   r: 71,
-//   g: 86,
-//   b: 102,
+//   r: 138,
+//   g: 138,
+//   b: 138,
 //   a: 1.0,
 // };
 
 // export const bwgrad11 = {
-//   r: 52,
-//   g: 65,
-//   b: 79,
+//   r: 125,
+//   g: 125,
+//   b: 125,
 //   a: 1.0,
 // };
 
 // export const bwgrad12 = {
-//   r: 39,
-//   g: 49,
-//   b: 61,
+//   r: 112,
+//   g: 112,
+//   b: 112,
 //   a: 1.0,
 // };
 
 // export const bwgrad13 = {
-//   r: 19,
-//   g: 25,
-//   b: 33,
+//   r: 99,
+//   g: 99,
+//   b: 99,
 //   a: 1.0,
 // };
 
-// // Retaining the original remaining shades from bwgrad14 to bwgrad20
 // export const bwgrad14 = {
+//   r: 86,
+//   g: 86,
+//   b: 86,
+//   a: 1.0,
+// };
+
+// export const bwgrad15 = {
+//   r: 73,
+//   g: 73,
+//   b: 73,
+//   a: 1.0,
+// };
+
+// export const bwgrad16 = {
+//   r: 60,
+//   g: 60,
+//   b: 60,
+//   a: 1.0,
+// };
+
+// export const bwgrad17 = {
+//   r: 47,
+//   g: 47,
+//   b: 47,
+//   a: 1.0,
+// };
+
+// export const bwgrad18 = {
 //   r: 34,
 //   g: 34,
 //   b: 34,
 //   a: 1.0,
 // };
 
-// export const bwgrad15 = {
+// export const bwgrad19 = {
 //   r: 21,
 //   g: 21,
 //   b: 21,
-//   a: 1.0,
-// };
-
-// export const bwgrad16 = {
-//   r: 10,
-//   g: 10,
-//   b: 10,
-//   a: 1.0,
-// };
-
-// export const bwgrad17 = {
-//   r: 5,
-//   g: 5,
-//   b: 5,
-//   a: 1.0,
-// };
-
-// export const bwgrad18 = {
-//   r: 3,
-//   g: 3,
-//   b: 3,
-//   a: 1.0,
-// };
-
-// export const bwgrad19 = {
-//   r: 2,
-//   g: 2,
-//   b: 2,
 //   a: 1.0,
 // };
 
@@ -310,7 +310,6 @@ export const green = {
 //   a: 1.0,
 // };
 
-// // Retaining original named colors
 // export const red = {
 //   r: 255,
 //   g: 0,
