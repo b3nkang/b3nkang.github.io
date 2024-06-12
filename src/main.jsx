0b46f0a1b7c153b4c72ad740d1ac1fb4b9cdff83
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-06-11 19:16:35 -0700

    impose min-width on main

--- src/main.jsx
+++ src/main.jsx
@@ -16,28 +16,38 @@ const RootComponent = () => {
 
   return (
     <React.StrictMode>
-      <div className="titlecard">
-        <div style={{
-            display: "flex",
-            justifyContent: "space-between",
-            flexGrow: 1,
-            alignItems: "center",
-            flexDirection: 'column',
-            height: "82vh"
-          }}>
+      <div style={{ minWidth: "600px"}}>
+        <div className="titlecard">
           <div style={{
-            display: "flex",
-            justifyContent: "center",
-            flexGrow: 1,
-            minWidth: "90vw",
-            maxWidth: "1400px"
-          }}>
-            <Titlecard findNewHeight={findNewHeight}/>
+              display: "flex",
+              justifyContent: "space-between",
+              flexGrow: 1,
+              alignItems: "center",
+              flexDirection: 'column',
+              height: "85vh",
+              flexBasis: "75%",
+              gap: "120px"
+            }}>
+            <div style={{
+              display: "flex",
+              justifyContent: "center",
+              flexGrow: 1,
+              minWidth: "90vw",
+              maxWidth: "1400px"
+            }}>
+              <Titlecard findNewHeight={findNewHeight}/>
+            </div>
+            <div style={{
+              flexBasis: "40%",
+              display: "flex",
+              alignItems: "end"
+            }}>
+              <Footer />
+            </div>
           </div>
-          <Footer />
         </div>
+        <App titlecardSpacerHeight={titlecardSpacerHeight}/>
       </div>
-      <App titlecardSpacerHeight={titlecardSpacerHeight}/>
     </React.StrictMode>
   )
 }
