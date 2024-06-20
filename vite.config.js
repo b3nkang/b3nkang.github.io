69ddde4a2ffc7377fdbb0a563deefc623b8f76a9
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-06-19 22:45:06 -0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-base: '/site/',
-plugins: [react()],
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/site/' : '/',
 })
