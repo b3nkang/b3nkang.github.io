1a640b99ccf5a29f3ec2cb9b1f8d2c129b268154
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-06-26 18:42:08 -0700

    refactor volunteering section with VolunteerItem

--- src/Volunteering.jsx
+++ src/Volunteering.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import ExperienceItem from './components/ExperienceItem';
 import HeaderTypography from './components/HeaderTypography';
+import VolunteerItem from './components/VolunteerItem';
 
 function Volunteering({ iconStyling, waveStyling}) {
     return (
@@ -9,7 +10,7 @@ function Volunteering({ iconStyling, waveStyling}) {
                 volunteering
             </HeaderTypography>
             <div className="wave4" style={{ marginTop: "20px", ...waveStyling}}>
-                <ExperienceItem
+                {/* <ExperienceItem
                     imgSrc={"/images/fsab_logo.jpeg"}
                     alt={"FSAB logo"}
                     name="Full Stack at Brown"
@@ -28,12 +29,39 @@ function Volunteering({ iconStyling, waveStyling}) {
                     period="May. 2024 – Aug. 2024"
                     location="(Remote) Pittsburgh, PA"
                     iconStyling={iconStyling}
+                    // bottomPadding={0}
+                    mt={4.5}
+                    // mb={4}
+                >
+                    Team: involveMINT. Using Angular, Express.
+                </ExperienceItem> */}
+                <VolunteerItem
+                    imgSrc={"/images/fsab_logo.jpeg"}
+                    alt={"FSAB logo"}
+                    name="Full Stack at Brown"
+                    title="Project Manager, Software Developer"
+                    period="Sept. 2023 – Current"
+                    location={`Providence, RI`}
+                    iconStyling={iconStyling}
+                    skillsArray={["react-dark", "typescript", "materialui", "nextjs-dark", "tailwindcss-dark","firebase-light","git","github-dark"]}
+                >
+                    Develop sites for local groups at Full Stack @ Brown, Brown’s pro-bono SWE club. Project Manager for Spring ’24. Created Brown JCA’s site with <code>TS/React/Material-UI</code> on 9-dev team, using <code>Firebase</code> for posts & image uploads.                
+                </VolunteerItem>
+                <VolunteerItem
+                    imgSrc={"/images/develop_for_good_logo.jpeg"}
+                    alt={"Develop for Good logo"}
+                    name="Develop for Good"
+                    title="Software Developer"
+                    period="May. 2024 – Aug. 2024"
+                    location="(Remote) Pittsburgh, PA"
+                    skillsArray={["angular",  "typescript","firebase-light", "docker"]}
+                    iconStyling={iconStyling}
                     bottomPadding={0}
                     mt={4.5}
                     mb={0}
                 >
                     Team: involveMINT. Using Angular, Express.
-                </ExperienceItem>
+                </VolunteerItem>
             </div>
         </div>
     )
